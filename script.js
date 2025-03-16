82cb3d54586a9dceb0db4097e67e3bac48aa888a
Temuulen Ganbold <tganbold01@gmail.com>
2025-03-16 17:43:15 +0800

    fix: language

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // script.js
 window.onload = function() {
     const urlParams = new URLSearchParams(window.location.search);
-    const name = urlParams.get('name') || "Guest"; // Default to 'Guest' if no name is provided
-    document.getElementById('greeting').textContent = `You're Invited, ${name}!`;
+    const name = urlParams.get('name') || "Эрхэм хүндэт зочин"; // Default to 'Guest' if no name is provided
+    document.getElementById('greeting').textContent = `Эрхэм хүндэт, ${name} таныг "инээд эмчилгээ" цуврал номын нээлтэнд хүрэлцэн ирэхийг урья.`;
 };
